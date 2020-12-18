006b30535322bfcd59ee7585381173b9475776d5
Krunal <krunaldhavle1999@gmail.com>
2020-12-19 02:09:52 +0530

    added player switching , score switching , active player switching

--- script.js
+++ script.js
@@ -6,16 +6,36 @@ const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
 //starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+let currentScore = 0;
+let activePlayer = 0;
+const scores = [0, 0];
 
 //rolling dice functionality
 btnRoll.addEventListener('click', function () {
   //generate random dice
-  const dice = Math.trunc(Math.random() * 6) + 1;
   diceEl.classList.remove('hidden');
+  const dice = Math.trunc(Math.random() * 6) + 1;
   diceEl.src = `dice-${dice}.png`;
+
+  if (dice !== 1) {
+    currentScore += dice;
+    document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
+  } else {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
 });
