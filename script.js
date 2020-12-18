a8022a2ad83c8a0158477cce79ade151b0973ce6
Krunal <krunaldhavle1999@gmail.com>
2020-12-19 01:23:01 +0530

    dice rolling

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 'use strict';
+// selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+//rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  //generate random dice
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+});
